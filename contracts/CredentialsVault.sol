a044f5d954add0dcf8ea29631452c4171339dbe5
goodness60disney <onyekachigoodness875@gmail.com>
2026-09-21 19:54:51 +0100

    Update CredentialsVault.sol

--- contracts/CredentialsVault.sol
+++ contracts/CredentialsVault.sol
@@ -1,3 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
-contract CredentialVault{} 
+contract CredentialVault { 
+
+struct Credential {
+     address student;
+     string program;
+     address issuer;
+     uint256 issuedAt;
+     bytes32 credentialHash;
+     bool revoked;
+    }
+} 
